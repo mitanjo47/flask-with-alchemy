65d6276917232c3ce0b2635c823159db4b9d44e6
John Doe <r.mitanjo@gmail.com>
2022-01-20 14:06:12 +0300

    First commit

---
+++ wsgi.py
@@ -0,0 +1,29 @@
+# wsgi.py
+# pylint: disable=missing-docstring
+
+BASE_URL = '/api/v1'
+
+from flask import Flask
+from config import Config
+app = Flask(__name__)
+app.config.from_object(Config)
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow 
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+from models import Product
+from flask_migrate import Migrate
+from schemas import many_product_schema
+
+migrate = Migrate(app, db)
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    return "Hello World!", 200
+
+@app.route(f'{BASE_URL}/products', methods=['GET'])
+def get_many_product():
+    products = db.session.query(Product).all() # SQLAlchemy request => 'SELECT * FROM products'
+    return many_product_schema.jsonify(products), 200
